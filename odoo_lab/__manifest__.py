537772143356ea3a44f693c3be870bf7067973a7
birajkhatiwada <bikh@odoo.com>
2022-12-12 19:05:10 +0000

    [ADD] Basic Module

---
+++ odoo_lab/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo lab',
+    'summary': """""",
+    'description':"""""",
+    'author':'Odoo',
+    'website':'https://www.odoo.com',
+    'category':'Training',
+    'version':'0.1',
+    'depends':['base'],
+    'data':[
+        
+        
+    ],
+    'demo':[
+        
+    ],
+}
