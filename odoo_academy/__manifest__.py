0801ff97efe4942ea51e9444db261008ffd113fc
birajkhatiwada <bikh@odoo.com>
2022-12-12 02:13:05 +0000

    Finished qweb and reports

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -22,8 +22,11 @@
         'views/session_views.xml',
         'views/sale_views_inherit.xml',
         'views/product_views_inherit.xml',
+        'wizard/sale_wizard_view.xml',
+        'report/session_report_templates.xml',
     ], 
     'demo': [
         'demo/academy_demo.xml',
-    ], 
+    ],
+    'license': 'OPL-1'
 }
