bef87d1df1d44b2884b37b87c663add70a8476c4
birajkhatiwada <bikh@odoo.com>
2022-12-14 19:19:09 +0000

    [ADD] Basic module

--- ny_pw_shoes/models/.ipynb_checkpoints/product_template-checkpoint.py
+++ ny_pw_shoes/models/.ipynb_checkpoints/product_template-checkpoint.py
@@ -1,12 +1,25 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+from odoo.exceptions import UserError, ValidationError
 
 class ProductTemplate(models.Model):
     
     _inherit = 'product.template'
     _description = ''
     
-    pair_per_case = fields.Integer(string='Pair Per Case', default=0)
+    pair_per_case = fields.Integer(string='Pair Per Case', help='this is a helper', default=0)
     price_per_case = fields.Monetary(string='Price Per Pair', default=0.00)
+    
+    
+    @api.onchange('pair_per_case', 'price_per_case')
+    def _onchange_sales_price(self):
+        if self.pair_per_case < 0:
+            raise UserError('Pair per case cannot be less than 0')
+        if self.price_per_case < 0.00:
+            raise UserError('Price cannot be less than $0.00')
+            
+        self.list_price = self.price_per_case * self.pair_per_case
+        
+    
+        
