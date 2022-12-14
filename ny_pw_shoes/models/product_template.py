5ee5261e18c542c769ffed098fc762a15d462415
birajkhatiwada <bikh@odoo.com>
2022-12-14 18:17:01 +0000

    [ADD] Basic Module

--- ny_pw_shoes/models/product_template.py
+++ ny_pw_shoes/models/product_template.py
@@ -1,4 +1,4 @@
-
+# -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
 
@@ -9,4 +9,4 @@ class ProductTemplate(models.Model):
     _description = ''
     
     pair_per_case = fields.Integer(string='Pair Per Case', default=0)
-    price_per_pair = fields.Monetary(string='Price Per Pair', default=0.00)
+    price_per_case = fields.Monetary(string='Price Per Pair', default=0.00)
