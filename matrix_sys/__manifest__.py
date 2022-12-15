c278f992b04722c47b2bd4f7e387b44042df40bd
birajkhatiwada <bikh@odoo.com>
2022-12-15 00:22:34 +0000

    [ADD] basic module

---
+++ matrix_sys/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Matrix System', 
+    'summary': """""", 
+    'description': """
+    """, 
+    'author': 'Odoo',
+    'website': 'https://www.odoo.com', 
+    'category': 'Training',
+    'version': '0.1', 
+    'depends': ['sale'], 
+    'data': [
+        
+    ], 
+    'demo': [
+        'demo/matrix_demo.xml',
+    ],
+    'license': 'OPL-1'
+}
