0069ffb2125e269cbe35c528ebd4385536619c0a
birajkhatiwada <bikh@odoo.com>
2022-12-20 19:55:58 +0000

    [ADD] basic module

---
+++ air-flow/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Air Flow', 
+    'summary': """""", 
+    'description': """""", 
+    'author': 'Odoo',
+    'website': 'https://www.odoo.com', 
+    'category': 'Training',
+    'version': '0.1', 
+    'depends': ['base'], 
+    'data': [
+        'report/sale_report_inherit.xml',
+    ], 
+    'license': 'OPL-1'
+}
