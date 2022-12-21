0ad74284052f975fce130a2fbb29c3cd4fa1cf5b
birajkhatiwada <bikh@odoo.com>
2022-12-21 06:09:01 +0000

    [UPDATE]

--- luxer_copy/models/.ipynb_checkpoints/sale_order-checkpoint.py
+++ luxer_copy/models/.ipynb_checkpoints/sale_order-checkpoint.py
@@ -5,7 +5,22 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
     
-    property_partner = fields.Many2one(comodel_name = 'res.partner',
-                                      string="Property Partner", tracking=1
-                                      )
-    test_num = fields.Char(string="Test", store=True, default="hello")
+    property_partner = fields.Many2one(comodel_name = 'res.partner')
+    def _create_invoices(self): 
+        so = super(SaleOrder, self)._create_invoices()
+        so['property_partner'] = self.property_partner
+        
+        # _logger.info(a.search_read([]))
+        # _logger.info(type(a))
+        return so
+    
+    
+    
+    
+    
+    @api.onchange('property_partner')
+    def _onchange_property_partner(self):
+        if self.property_partner:
+            self.partner_shipping_id = self.property_partner
+        else:
+            self.partner_shipping_id = self.partner_id
