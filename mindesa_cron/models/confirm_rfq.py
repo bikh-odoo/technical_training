7dca20e5e8e5851905d05a0a1b110dd71747837e
birajkhatiwada <bikh@odoo.com>
2022-12-22 23:30:23 +0000

    [FINAL UPDATE]

--- mindesa_cron/models/confirm_rfq.py
+++ mindesa_cron/models/confirm_rfq.py
@@ -6,4 +6,7 @@ class ConfirmRfq(models.Model):
     _name = "mindesa_cron.confirm_rfq"
     
     def _confirm_rfq(self):
-        pass
+        purchase_orders = self.env['purchase.order'].search([['partner_id','=',1],['create_uid','=',1]])
+        for purchase_order in purchase_orders:
+            purchase_order.button_confirm()
+            
