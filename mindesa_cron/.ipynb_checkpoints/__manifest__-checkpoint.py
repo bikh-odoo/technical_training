8b8b7d5c6e176f5bbd3e9773887bbfd01a93e709
birajkhatiwada <bikh@odoo.com>
2022-12-22 19:03:48 +0000

    [ADD] basic module

---
+++ mindesa_cron/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Mindesa Cron',
+    'summary': """""",
+    'description': """""",
+    'author': 'Odoo',
+    'website': 'https://www.odoo.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['sale'],
+    'data':[
+        'views/confirm_rfq_views.xml',
+    ],
+}
---
+++ mindesa_cron/models/.ipynb_checkpoints/confirm_rfq-checkpoint.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class ConfirmRfq(models.Model):
+    _name = "mindesa_cron.confirm_rfq"
+    
+    def _confirm_rfq(self):
+        pass
